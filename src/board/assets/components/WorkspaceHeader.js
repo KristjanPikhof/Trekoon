5cde0bacbd96e801afb93b504d2a038480577ba5
KristjanPikhof <kristjan.pikhof@gmail.com>
2026-03-15 15:18:38 +0200

    Update render workspace header

--- src/board/assets/components/WorkspaceHeader.js
+++ src/board/assets/components/WorkspaceHeader.js
@@ -5,6 +5,7 @@ export function renderWorkspaceHeader(context) {
     neutralChipClasses,
     renderEpicCountSummary,
     renderIcon,
+    renderStatusBadge,
     sectionLabelClasses,
     selectedEpic,
     snapshotEpics,
