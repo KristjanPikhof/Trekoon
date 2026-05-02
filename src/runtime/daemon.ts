0acb1e1cc8ca01e1bd719e6e3bcbdf41074bec25
KristjanPikhof <kristjan.pikhof@gmail.com>
2026-05-02 18:18:55 +0300

    Import safe error formatter in daemon
    - Import the shared safe error formatter to avoid raw error leaks

--- src/runtime/daemon.ts
+++ src/runtime/daemon.ts
@@ -1,6 +1,7 @@
 import { chmodSync, existsSync, mkdirSync, unlinkSync, statSync } from "node:fs";
 import { connect, createServer, type Server, type Socket } from "node:net";
 import { dirname } from "node:path";
+import { safeErrorMessage } from "../commands/error-utils";
 import { closeCachedDatabases } from "../storage/database";
 import { resolveStoragePaths } from "../storage/path";
 
